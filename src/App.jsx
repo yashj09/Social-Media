caaf12d3f56fc9c5bf5e3b44ad4a6feb0056913a
yashj09 <yashj8858@gmail.com>
2024-01-24 01:39:49 +0530

    added wallet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from "@dynamic-labs/sdk-react-core";
+import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 
-const App = () => {
-  return <h1>Hello world!</h1>;
-};
+const App = () => (
+  <DynamicContextProvider
+    settings={{
+      environmentId: "daa53108-7e85-4cc3-aabf-4032b12f4bd9",
+      walletConnectors: [EthereumWalletConnectors],
+    }}
+  >
+    <DynamicWidget />
+  </DynamicContextProvider>
+);
 
 export default App;
